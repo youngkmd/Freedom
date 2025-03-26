dadc70dc3b4d5e1fa50cc5419b38d0da67593e23
youngkmd <wpinstall34@gmail.com>
2025-03-26 06:00:05 +0100

    youngkmd/Freedom

--- public/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-require_once __DIR__ . '/../vendor/autoload.php'; // Assuming Composer autoloading
-require_once __DIR__ . '/../config/config.php';
-
-use App\Controllers\FileController;
-
-$controller = new FileController();
-$controller->handleRequest();
